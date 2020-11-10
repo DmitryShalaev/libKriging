3eff49cb05811501e121f45eb268c603f9edd526
yannrichet <richetyann@gmail.com>
2020-11-10 16:25:40 +0100

    refact + add optim test with true LL obj

--- bindings/R/rlibkriging/src/bench.cpp
+++ bindings/R/rlibkriging/src/bench.cpp
@@ -36,7 +36,7 @@ Rcpp::List bench_qr(int n, arma::mat X) {
 }
 
 // [[Rcpp::export]]
-double bench_loglik(int n, Rcpp::List ordinaryKriging, arma::vec theta) {
+double bench_LogLik(int n, Rcpp::List ordinaryKriging, arma::vec theta) {
   if (!ordinaryKriging.inherits("OrdinaryKriging"))
     Rcpp::stop("Input must be a OrdinaryKriging object.");
   SEXP impl_ok = ordinaryKriging.attr("object");
@@ -48,7 +48,7 @@ double bench_loglik(int n, Rcpp::List ordinaryKriging, arma::vec theta) {
 }
 
 // [[Rcpp::export]]
-arma::vec bench_loglikgrad(int n, Rcpp::List ordinaryKriging, arma::vec theta) {
+arma::vec bench_LogLikGrad(int n, Rcpp::List ordinaryKriging, arma::vec theta) {
   if (!ordinaryKriging.inherits("OrdinaryKriging"))
     Rcpp::stop("Input must be a OrdinaryKriging object.");
   SEXP impl_ok = ordinaryKriging.attr("object");
@@ -60,21 +60,36 @@ arma::vec bench_loglikgrad(int n, Rcpp::List ordinaryKriging, arma::vec theta) {
 }
 
 // [[Rcpp::export]]
-arma::mat bench_optim(arma::vec x0) {
+Rcpp::List bench_OptimLL(Rcpp::List ordinaryKriging, arma::vec x0) {
+  if (!ordinaryKriging.inherits("OrdinaryKriging"))
+    Rcpp::stop("Input must be a OrdinaryKriging object.");
+  SEXP impl_ok = ordinaryKriging.attr("object");
+  Rcpp::XPtr<OrdinaryKriging> ok(impl_ok);
+  
+  OrdinaryKriging::Parameters parameters{0,false,x0,true};
+  ok->fit(parameters, ok->y(), ok->X(), ok->regmodel(), false);
+  
+  return Rcpp::List::create(Rcpp::Named("fun") = ok->funs(),
+                            Rcpp::Named("grad") = ok->grads()); 
+}
+
+
+// [[Rcpp::export]]
+arma::mat bench_OptimRosenbrock(arma::vec x0) {
   Bench* b = new Bench(1);
   Rcpp::XPtr<Bench> impl_ptr(b);
   return impl_ptr->OptimRosenbrock(x0);
 }
 
 // [[Rcpp::export]]
-double f_optim(arma::vec x) {
+double bench_Rosenbrock(arma::vec x) {
   Bench* b = new Bench(1);
   Rcpp::XPtr<Bench> impl_ptr(b);
   return impl_ptr->Rosenbrock(x);
 }
 
 // [[Rcpp::export]]
-arma::vec grad_optim(arma::vec x0) {
+arma::vec bench_RosenbrockGrad(arma::vec x0) {
   Bench* b = new Bench(1);
   Rcpp::XPtr<Bench> impl_ptr(b);
   return impl_ptr->RosenbrockGrad(x0);
