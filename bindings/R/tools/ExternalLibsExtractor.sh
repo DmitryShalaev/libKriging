fbdf7a9e6cd4a4e67b51e1857a9a5ac8442427a2
Pascal Havé <hpwxf@haveneer.com>
2023-07-09 18:53:22 +0200

    WIP

--- bindings/R/tools/ExternalLibsExtractor.sh
+++ bindings/R/tools/ExternalLibsExtractor.sh
@@ -11,18 +11,17 @@ BASEDIR=$(dirname "$0")
 BASEDIR=$(cd "$BASEDIR" && pwd -P)
 
 TMPDIR=$(mktemp -d)
-ARMA_LIBS=$(cmake -DLIBKRIGING_PATH="${LIBKRIGING_PATH}" -B "${TMPDIR}" "${BASEDIR}")## | sed -n -r -e 's/^-- EXTERNAL_LIBS=(.*)$/\1/p')
-echo ${ARMA_LIBS}
+ARMA_LIBS=$(cmake -DLIBKRIGING_PATH="${LIBKRIGING_PATH}" -B "${TMPDIR}" "${BASEDIR}" | sed -n -r -e 's/^-- EXTERNAL_LIBS=(.*)$/\1/p')
 ## avoid *-NOTFOUND to be displayed:
-##ARMA_LIBS=$(echo "$ARMA_LIBS" | sed -e 's/[^ ]*-NOTFOUND[^ ]*//g')
+ARMA_LIBS=$(echo "$ARMA_LIBS" | sed -e 's/[^ ]*-NOTFOUND[^ ]*//g')
 
 # Add double quotes around each path to protect them except when it starts with a - which should be a (composed) option like "-framework Accelerate"
-## echo "${ARMA_LIBS}" | while read l
-##   do
-##     if [[ "$l" == "-"* ]]; then
-##       echo -n " $l"
-##     else
-##       echo -n " \"$l\"";
-##     fi
-##   done
-## rm -fr "${TMPDIR}"
+echo "${ARMA_LIBS}" | while read l
+  do
+    if [[ "$l" == "-"* ]]; then
+      echo -n " $l"
+    else
+      echo -n " \"$l\"";
+    fi
+  done
+rm -fr "${TMPDIR}"
